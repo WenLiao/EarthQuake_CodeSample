749e943e30d4564e72c1750d156b75ae0abcd676
jimliao <jimliao@jimliaotekimbp.pk5001z>
2016-04-14 18:24:28 -0700

    Initial Commit

---
+++ SampleCodeEbay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleCodeEbay
+//
+//  Created by jimliao on 2016/4/14.
+//  Copyright © 2016年 jimliao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SampleCodeEbay/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SampleCodeEbay
+//
+//  Created by jimliao on 2016/4/14.
+//  Copyright © 2016年 jimliao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
